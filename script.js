7703e208e9a2f1c0452db36c356950a6fc7a7c09
HWG - Jakub Hewig <hwgpraca@gmail.com>
2023-05-24 11:24:41 +0200

    changes

--- script.js
+++ script.js
@@ -1,96 +1,90 @@
-const currentNumber = document.querySelector('.currentNumber');
+const currentNumber = document.querySelector(".currentNumber");
 
-const previousNumber = document.querySelector('.previousNumber p');
+const previousNumber = document.querySelector(".previousNumber p");
 
-const mathSign = document.querySelector('.mathSign');
+const mathSign = document.querySelector(".mathSign");
 
-const numbersButtons = document.querySelectorAll('.number');
+const numbersButtons = document.querySelectorAll(".number");
 
-const operatorsButtons = document.querySelectorAll('.operator');
+const operatorsButtons = document.querySelectorAll(".operator");
 
-const equalsButton = document.querySelector('.equals');
+const equalsButton = document.querySelector(".equals");
 
-const clearButton = document.querySelector('.clear');
+const clearButton = document.querySelector(".clear");
 
-const calculatorHistory = document.querySelector('.history');
+const calculatorHistory = document.querySelector(".history");
 
-const historyBtn = document.querySelector('.history-btn');
+const historyBtn = document.querySelector(".history-btn");
 
-let result = '';
+let result = "";
 
+function displayNumbers() {
+  if (this.textContent === "." && currentNumber.innerHTML.includes(".")) return;
+  if (this.textContent === "." && currentNumber.innerHTML === "")
+    return (currentNumber.innerHTML = ".0");
 
-function displayNumbers () {
-        if(this.textContent === '.' && currentNumber.innerHTML.includes('.')) return;
-        if(this.textContent === '.' && currentNumber.innerHTML === '') return currentNumber.innerHTML = '.0'
-
-        currentNumber.innerHTML += this.textContent;
+  currentNumber.innerHTML += this.textContent;
 }
 
-function operate () {
-    if(currentNumber.innerHTML === '' && this.textContent ==='-'){
-        currentNumber.innerHTML = '-';
-        return;
-    }
-    
+function operate() {
+  if (currentNumber.innerHTML === "" && this.textContent === "-") {
+    currentNumber.innerHTML = "-";
+    return;
+  } else if (currentNumber.innerHTML === "") {
+    return;
+  }
 
-     else if (currentNumber.innerHTML === '') {
-        return;
-     }
-
-     if(mathSign.innerHTML !== '') {
-         showResult();
-     }
-     previousNumber.innerHTML = currentNumber.innerHTML;
-     mathSign.innerHTML = this.textContent;
-     currentNumber.innerHTML ='';
+  if (mathSign.innerHTML !== "") {
+    showResult();
+  }
+  previousNumber.innerHTML = currentNumber.innerHTML;
+  mathSign.innerHTML = this.textContent;
+  currentNumber.innerHTML = "";
 }
 
-function showResult () {
-    if(previousNumber.innerHTML === '' || currentNumber.innerHTML === '') return;
+function showResult() {
+  if (previousNumber.innerHTML === "" || currentNumber.innerHTML === "") return;
 
-    let a = Number(currentNumber.innerHTML);
-    let b = Number(previousNumber.innerHTML);
-    let operator = mathSign.innerHTML;
+  let a = Number(currentNumber.innerHTML);
+  let b = Number(previousNumber.innerHTML);
+  let operator = mathSign.innerHTML;
 
+  switch (operator) {
+    case "+":
+      result = a + b;
+      break;
+    case "-":
+      result = b - a;
+      break;
+    case "x":
+      result = a * b;
+      break;
+    case ":":
+      result = b / a;
+      break;
+    case "^":
+      result = a ** b;
+      break;
+  }
 
-    switch(operator) {
-        case '+':
-        result = a + b;
-        break;
-        case '-':
-        result = b - a;
-        break;
-        case 'x':
-        result = a * b;
-        break;
-        case ':':
-        result = b / a;
-        break;
-        case '^':
-        result = a ** b;
-        break;
-    }
-
-    currentNumber.innerHTML = result;
-    previousNumber.innerHTML ='';
-    mathSign.innerHTML ='';
+  currentNumber.innerHTML = result;
+  previousNumber.innerHTML = "";
+  mathSign.innerHTML = "";
 }
 
-function clearScreen () {
-    result = '';
-    currentNumber.innerHTML = '';
-    previousNumber.innerHTML = '';
-    mathSign.innerHTML = '';
-
+function clearScreen() {
+  result = "";
+  currentNumber.innerHTML = "";
+  previousNumber.innerHTML = "";
+  mathSign.innerHTML = "";
 }
 
- operatorsButtons.forEach((button) => button.addEventListener('click', operate))
+operatorsButtons.forEach((button) => button.addEventListener("click", operate));
 
- equalsButton.addEventListener('click', showResult);
+equalsButton.addEventListener("click", showResult);
 
+clearButton.addEventListener("click", clearScreen);
 
- clearButton.addEventListener('click', clearScreen);
-
- numbersButtons.forEach((button) => {
-     button.addEventListener('click', displayNumbers)
- })
+numbersButtons.forEach((button) => {
+  button.addEventListener("click", displayNumbers);
+});
